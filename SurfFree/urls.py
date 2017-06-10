b83062d0ccce836de54ead995ae68a0002a3bc8f
hexnor <cse.ykr@gmail.com>
2017-06-11 03:08:37 +0530

    all

--- SurfFree/urls.py
+++ SurfFree/urls.py
@@ -21,7 +21,7 @@ from route import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^proxy/(?P<url>.*)', views.index, name='proxy')
+    url(r'^proxy/(?P<url>.*)', views.index, name='proxy'),
     url(r'^',views.process),
 
 
